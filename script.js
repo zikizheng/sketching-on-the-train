cde00c8f4889e15d09cb9e6f9a17c5969a2a7065
Ziki <ziki.zheng@gmail.com>
2024-02-26 04:34:28 +1100

    car 2

--- script.js
+++ script.js
@@ -44,3 +44,49 @@ container.addEventListener('wheel', (event) => {
     container.scrollLeft += deltaY;
   }
 });
+
+// Function to change the color of the text
+function changeTextColor(color, elementID) {
+    var textElement = document.getElementById(elementID);
+    textElement.style.transition = 'color 0.3s ease-in-out';
+    textElement.style.color = color;
+}
+
+document.getElementById('window-1').addEventListener('click', function() {
+    changeTextColor('black', 'window-1-text');
+});
+
+document.getElementById('window-1').addEventListener('mouseout', function() {
+    changeTextColor('', 'window-1-text');
+});
+
+document.getElementById('auntie').addEventListener('click', function() {
+    changeTextColor('black', 'auntie-h2');
+    changeTextColor('black', 'auntie-text');
+});
+
+document.getElementById('auntie').addEventListener('mouseout', function() {
+    changeTextColor('', 'auntie-h2');
+    changeTextColor('', 'auntie-text');
+});
+
+document.getElementById('couple').addEventListener('click', function() {
+    changeTextColor('black', 'couple-h2');
+    changeTextColor('black', 'couple-text');
+});
+
+document.getElementById('couple').addEventListener('mouseout', function() {
+    changeTextColor('', 'couple-h2');
+    changeTextColor('', 'couple-text');
+});
+
+
+document.getElementById('bag-1').addEventListener('click', function() {
+    this.classList.remove('clicked');
+    // Add the 'clicked' class to trigger the spin animation
+    void this.offsetWidth; // Trigger reflow to restart the animation
+    this.classList.add('clicked');
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 1000);
+});
